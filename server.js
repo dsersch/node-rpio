e382be19144a09a248c4155e6d4ba70d27195879
dave <dsersch640@gmail.com>
2018-06-11 14:01:37 -0700

    home test

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const
     rpio = require('rpio'),
     PORT = 3001
 
-
+app.get('/', (req, res) => {
+    res.send("it's working....")
+})
 
 app.listen(PORT, (err) => {
     console.log(err || `server running on ${PORT}`)
