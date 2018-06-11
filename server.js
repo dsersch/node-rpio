e47519160e7e03210210ab2077981614d96c784e
dave <dsersch640@gmail.com>
2018-06-11 16:21:41 -0700

    first relay test

--- server.js
+++ server.js
@@ -9,6 +9,17 @@ app.get('/', (req, res) => {
 })
 
 app.get('/on/:id', (req, res) => {
+
+    rpio.open(2, rpio.OUTPUT, rpio.LOW)
+
+    for (let i = 0; i < 5; i++) {
+        rpio.write(2, rpio.HIGH);
+        rpio.sleep(500);
+
+        rpio.write(2, rpio.LOW)
+        rpio.sleep(500)
+    }
+
     res.json({message: `You want to turn relay ${req.params.id} on...`})
 })
 
