7dcc06c6411cdc8c732d0c233ca91dbe052cab6b
dave <dsersch640@gmail.com>
2018-06-12 11:06:36 -0700

    pin fix

--- gpio-test-2.js
+++ gpio-test-2.js
@@ -1,11 +1,11 @@
 var rpio = require('rpio')
 
-rpio.open(5, rpio.OUTPUT, rpio.LOW)
+rpio.open(29, rpio.OUTPUT, rpio.LOW)
 
 for (var i = 0; i < 5; i++) {
-    rpio.write(5, rpio.HIGH)
+    rpio.write(29, rpio.HIGH)
     rpio.sleep(500)
 
-    rpio.write(5, rpio.LOW)
+    rpio.write(29, rpio.LOW)
     rpio.sleep(500)
 }
