7684c101ad4ea2134eb3a3865319f5bda5b04d7e
dave <dsersch640@gmail.com>
2018-06-12 11:11:14 -0700

    one pin test

--- gpio-test-2.js
+++ gpio-test-2.js
@@ -2,10 +2,7 @@ var rpio = require('rpio')
 
 rpio.open(29, rpio.OUTPUT, rpio.LOW)
 
-for (var i = 0; i < 5; i++) {
-    rpio.write(29, rpio.HIGH)
-    rpio.sleep(500)
+rpio.write(29, rpio.HIGH)
+rpio.sleep(500)
 
-    rpio.write(29, rpio.LOW)
-    rpio.sleep(500)
-}
+rpio.write(29, rpio.LOW)
